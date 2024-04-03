79524740760773d69581e6822c3f9b7dd2ed6c6d
andrefaquineti <andrefaquineti@gmail.com>
2024-04-03 13:47:14 -0300

    24/04/03 calculadora php

---
+++ calculadora_soma_php/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculadora PHP</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/teste/darkmode_geral.css">
+</head>
+<body>
+    <p><h1><a href="/teste">Pagina Inicial</a></h1></p>
+    <form method="post" action="">
+        <label for="num1">Numero 1:</label>
+        <input type="int" name="num1"> <br><br>
+        <label for="num2">Numero 2:</label>
+        <input type="int" name="num2"> <br><br>
+        <input type="submit"> <br><br>
+    </form>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (!empty($_POST['num1']) && !empty($_POST['num2'])) {
+            $num1 = $_POST['num1'];
+            $num2 = $_POST['num2'];
+        }
+        if (!isset($num1) OR !isset($num2)) {
+            $num1 = 0;
+            $num2 = 0;
+        }
+        $resultado = $num1 + $num2;
+        echo 'Resultado: ' . $resultado;
+    }
+    ?>
+</body>
+</html>
