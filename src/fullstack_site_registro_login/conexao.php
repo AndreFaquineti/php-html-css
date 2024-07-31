9f7b287011076134a0e25892cbbae9884d4fb380
AndreFaquineti <142813791+AndreFaquineti@users.noreply.github.com>
2024-07-31 19:32:37 -0300

    Teste conexao.php

--- src/fullstack_site_registro_login/conexao.php
+++ src/fullstack_site_registro_login/conexao.php
@@ -1,4 +1,24 @@
 <?php
+$uri = "mysql://avnadmin:AVNS_Eh3MstTzS3bRKma2cM9@mysql-portifolio-php-html-css.i.aivencloud.com:27320/defaultdb?ssl-mode=REQUIRED";
+
+$fields = parse_url($uri);
+
+// build the DSN including SSL settings
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];;
+$conn .= ";dbname=defaultdb";
+$conn .= ";sslmode=verify-ca;sslrootcert='D:/absolute/path/to/ssl/certs/ca.pem'";
+
+try {
+    $db = new PDO($conn, $fields["user"], $fields["pass"]);
+
+    $stmt = $db->query("SELECT VERSION()");
+    print($stmt->fetch()[0]);
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+/*
     $servername="mysql";
     $username="root";
     $password="secret";
@@ -11,4 +31,5 @@
     } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     }
+*/
 ?>
